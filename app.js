a4690dd129779d447abb7fac8071f1076904a677
ftoledo43 <franciscojaviertoledocollante@gmail.com>
2024-11-17 13:41:30 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+(() => {
+    const a = 10;
+    console.log(a);
+})();
+// (() => {
+//   const a: number = 10;
+// let b: number = 10;
+// let c:string;
+// c = 3.141516;
+// c = {};
+// c = [];
+// c = true;
+// function sayHello(msg: string) {
+//   console.log(msg + '' + "Francisco")
+//   msg.
+// }
+// }
+// )()
